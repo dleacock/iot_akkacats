e6c7fc6adc241ad39c45c0fea2a8d3cda2f57ac3
davidleacock <dave.leacock@gmail.com>
2022-05-24 13:38:45 -0700

    implement init test

--- src/test/actors/DeviceTest.scala
+++ src/test/actors/DeviceTest.scala
@@ -1,8 +1,8 @@
 package actors
 
-import actors.Device.Command.{InitializeDevice, GetDeviceState, AlertDevice}
+import actors.Device.Command.{AlertDevice, GetDeviceState, InitializeDevice}
 import actors.Device.Response.{DeviceInitializedResponse, DeviceStateUpdatedResponse, GetDeviceStateResponse}
-import actors.Device.{Command, DeviceInitialized, Device, DeviceAlerted, Event, Response}
+import actors.Device.{Command, Device, DeviceAlerted, DeviceInitialized, Event, Inactive, Response, State}
 import akka.actor.testkit.typed.scaladsl.{LogCapturing, ScalaTestWithActorTestKit}
 import akka.persistence.testkit.scaladsl.EventSourcedBehaviorTestKit
 import com.typesafe.config.ConfigFactory
@@ -21,44 +21,45 @@ class DeviceTest
     with LogCapturing {
 
   private val id: String = UUID.randomUUID().toString
-//  private val eventSourcedTestKit: EventSourcedBehaviorTestKit[Command, Event, Device] =
-//    EventSourcedBehaviorTestKit[Command, Event, Device](
-//      system,
-//      Device(id))
+  private val name: String = "test_device"
+  private val eventSourcedTestKit: EventSourcedBehaviorTestKit[Command, Event, State] =
+    EventSourcedBehaviorTestKit[Command, Event, State](
+      system,
+      actors.Device(id, name)) // TODO fix namespace collision
 
   override protected def beforeEach(): Unit = {
     super.beforeEach()
-//    eventSourcedTestKit.clear()
+    eventSourcedTestKit.clear()
   }
 
-//  "A Device" must {
-//    val initialState = "initial_state"
-//    val updatedState = "updated_state"
-//
-//    "be created with initial state" in {
-//      val result = eventSourcedTestKit.runCommand[Response](replyTo => InitializeDevice(id, initialState, replyTo))
-//      result.reply shouldBe DeviceInitializedResponse(id)
-//      result.event shouldBe DeviceInitialized(id, initialState)
-//      result.stateOfType[Device].state shouldBe initialState
-//      result.stateOfType[Device].id shouldBe id
-//    }
-//
-//    "handle update" in {
-//      eventSourcedTestKit.runCommand[Response](replyTo => InitializeDevice(id, initialState, replyTo))
-//
-//      val result = eventSourcedTestKit.runCommand[Response](replyTo => UpdateDevice(id, updatedState, replyTo))
-//      result.reply shouldBe DeviceStateUpdatedResponse(Success(Device(id, updatedState)))
-//      result.event shouldBe DeviceStateUpdated(updatedState)
-//      result.stateOfType[Device].state shouldBe updatedState
-//      result.stateOfType[Device].id shouldBe id
-//    }
-//
-//    "handle get" in {
-//      eventSourcedTestKit.runCommand[Response](replyTo => InitializeDevice(id, initialState, replyTo))
-//      eventSourcedTestKit.runCommand[Response](replyTo => UpdateDevice(id, updatedState, replyTo))
-//
-//      val result = eventSourcedTestKit.runCommand[Response](replyTo => GetDeviceState(id, replyTo))
-//      result.reply shouldBe GetDeviceStateResponse(Some(Device(id, updatedState)))
-//    }
-//  }
+  "A Device" must {
+    val initialState = "initial_state"
+    val updatedState = "updated_state"
+    val device = Device(id, name)
+
+    "be created in inactive state" in {
+      val result = eventSourcedTestKit.runCommand[Response](replyTo => GetDeviceState(id, replyTo))
+      result.reply shouldBe GetDeviceStateResponse(Some(Inactive(device)))
+      result.stateOfType[Inactive].device shouldBe device
+    }
+    //
+    //    "handle update" in {
+    //      eventSourcedTestKit.runCommand[Response](replyTo => InitializeDevice(id, initialState, replyTo))
+    //
+    //      val result = eventSourcedTestKit.runCommand[Response](replyTo => UpdateDevice(id, updatedState, replyTo))
+    //      result.reply shouldBe DeviceStateUpdatedResponse(Success(Device(id, updatedState)))
+    //      result.event shouldBe DeviceStateUpdated(updatedState)
+    //      result.stateOfType[Device].state shouldBe updatedState
+    //      result.stateOfType[Device].id shouldBe id
+    //    }
+    //
+    //    "handle get" in {
+    //      eventSourcedTestKit.runCommand[Response](replyTo => InitializeDevice(id, initialState, replyTo))
+    //      eventSourcedTestKit.runCommand[Response](replyTo => UpdateDevice(id, updatedState, replyTo))
+    //
+    //      val result = eventSourcedTestKit.runCommand[Response](replyTo => GetDeviceState(id, replyTo))
+    //      result.reply shouldBe GetDeviceStateResponse(Some(Device(id, updatedState)))
+    //    }
+    //  }
+  }
 }
