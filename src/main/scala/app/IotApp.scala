ee6ada01d6ec7b17978786df4d4930a65b459c2b
davidleacock <dave.leacock@gmail.com>
2022-06-04 13:23:13 -0700

    Create bones for HttpNotifier, experiment with running it

--- src/main/scala/app/IotApp.scala
+++ src/main/scala/app/IotApp.scala
@@ -1,8 +1,28 @@
 package app
 
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import notifier.HttpNotifier
+
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
+
 object IotApp {
 
   def main(args: Array[String]): Unit = {
     println("*** Running ***")
+
+    implicit val system: ActorSystem[_] =
+      ActorSystem(Behaviors.empty, "IotApp")
+    implicit val ec: ExecutionContext = system.executionContext
+
+    val notifier = new HttpNotifier("http://www.google.ca")
+    notifier.sendNotification.onComplete {
+      case Success(value) => system.log.info(s"Success: App got back $value")
+      case Failure(exception) =>
+        system.log.info(s"Failure: App got ${exception.getMessage}")
+    }
+
+
   }
 }
--- src/main/scala/notifier/HttpNotifier.scala
+++ src/main/scala/notifier/HttpNotifier.scala
@@ -1,15 +1,50 @@
 package notifier
 
 import akka.Done
+import akka.actor.typed.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, StatusCodes}
+import org.slf4j.Logger
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-object HttpNotifier extends Notifier[Done]{
-  override def sendNotification: Future[Done] = ???
+class HttpNotifier(
+  url: String
+)(implicit val system: ActorSystem[_],
+  val ec: ExecutionContext)
+    extends Notifier[Done] {
+
+  val logger: Logger = system.log
+
+  override def sendNotification: Future[Done] = {
+    logger.info(s"Sending notification to $url")
+    Http()
+      .singleRequest(HttpRequest(uri = s"$url"))
+      .map { resp =>
+        resp.discardEntityBytes()
+        resp.status
+      }
+      .flatMap {
+        case StatusCodes.OK => {
+          logger.info("Got back ok")
+          Future.successful(Done)
+        }
+        case StatusCodes.BadRequest => {
+          logger.info("Got back bad request")
+          Future.successful(Done)
+        }
+        case _ => {
+          logger.info("NO IDEA")
+          Future.successful(Done)
+        }
+      }
+      .recoverWith { ex =>
+        logger.info("WTF")
+        Future.failed(new RuntimeException("bad stuff"))
+
+      }
+  }
 
   override def getType: String = "HttpNotifier"
 
-  // TODO inject in akka http
-  def apply(): Unit ={
-  }
 }
