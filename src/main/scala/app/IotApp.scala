89869cc92dfe118402bc39e4bc018dcb474e51b9
davidleacock <dave.leacock@gmail.com>
2022-06-05 11:48:47 -0700

    Start to build out app from main - WIP

--- src/main/scala/app/IotApp.scala
+++ src/main/scala/app/IotApp.scala
@@ -1,28 +1,59 @@
 package app
 
-import akka.actor.typed.ActorSystem
+import actors.{NotifierActor, PersistentDevice}
+import akka.Done
+import akka.actor.typed.{ActorSystem, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
-import notifier.HttpNotifier
+import akka.cluster.sharding.typed.scaladsl.{ClusterSharding, Entity}
+import akka.persistence.typed.PersistenceId
+import notifier.{HttpNotifier, Notifier}
+import service.{DefaultIotDeviceService, IotDeviceService}
 
-import scala.concurrent.ExecutionContext
+import java.util.UUID
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object IotApp {
 
-  def main(args: Array[String]): Unit = {
-    println("*** Running ***")
+  def main(args: Array[String]): Unit =
+    ActorSystem[Nothing](appBehavior(), "iot-app")
 
-    implicit val system: ActorSystem[_] =
-      ActorSystem(Behaviors.empty, "IotApp")
-    implicit val ec: ExecutionContext = system.executionContext
+  private def appBehavior(): Behavior[Nothing] = {
+    Behaviors.setup[Nothing] { context =>
+      implicit val system: ActorSystem[_] = context.system
 
-    val notifier = new HttpNotifier("http://www.google.ca")
-    notifier.sendNotification.onComplete {
-      case Success(value) => system.log.info(s"Success: App got back $value")
-      case Failure(exception) =>
-        system.log.info(s"Failure: App got ${exception.getMessage}")
+      val service: IotDeviceService = initService
+
+      service.registerDevice(UUID.randomUUID().toString)
+
+      Behaviors.empty
+    }
+  }
+
+  private def initService(implicit system: ActorSystem[_]): IotDeviceService = {
+    val sharding = ClusterSharding(system)
+
+    // TODO how is ClusterSharding knowing what ID to use to map to actor?
+    // TODO is this the right location for it?
+    sharding.init(Entity(typeKey = PersistentDevice.TypeKey) { entityContext =>
+      PersistentDevice(
+        PersistenceId(entityContext.entityTypeKey.name, entityContext.entityId)
+      )
+    })
+
+    val notifier = new Notifier[Done] {
+      override def sendNotification: Future[Done] = {
+        system.log.info("Sending notification")
+        Future.successful(Done)
+      }
+
+      override def getType: String = "dummy"
     }
 
+    val notifierActor: Behavior[NotifierActor.NotifierMessage] = NotifierActor(notifier)
+
+    new DefaultIotDeviceService(sharding, notifierActor)
 
   }
 }
